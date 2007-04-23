41ab52030a2b525023365e408448c257e8b43975
wyatt.lee.baldwin <wyatt.lee.baldwin@localhost>
2007-04-23 02:33:31 +0000

    Added helpers for pagination and creating form fields for columns. Updated ``nav_list_for_member`` to optionally output the links as HTML. blog_post_announcement had some tiny changes which are piggybacking on this commit.
    * nav_list_for_member
      * Added keyword arg ``terse_link_text``--when this is true, the link text
        will be just "Show", "Edit", etc; when it's false, the link text will
        include the member name, like "Show Monkey".
      * Added keyword arg ``join_with``--when this is specified, instead of
        returning a dict of links, join the links together and return an HTML
        string.
    
    * (form_field_for_col): Added--returns an HTML form element for a given
      (SQL) ``column`` and ``member``
    
    * (nice_name_for_col): Added--returns a nice name for the given column, where
      "nice" is defined as replacing underscores with spaces and capitalizing
    
    * (get_prev_next_per_page): Added--Given a ``paginator`` object (e.g., from
      WebHelpers), returns the previous page number, the next page number, and
      the number of items per page

--- restler/helpers.py
+++ restler/helpers.py
@@ -1,11 +1,64 @@
 """Restler helper functions."""
 from webhelpers import link_to, button_to, url, tag
+from webhelpers import text_field, text_area, check_box
+from sqlalchemy import types
 
-__all__ = ['make_nav_menu',
-           'make_nav_list',
-           'nav_list_for_resource',
-           'nav_list_for_collection',
-           'nav_list_for_member']
+__all__ = [
+    'make_nav_menu',
+    'make_nav_list',
+    'nav_list_for_resource',
+    'nav_list_for_collection',
+    'nav_list_for_member',
+    'form_field_for_col',
+    'nice_name_for_col',
+    'get_prev_next_per_page',
+]
+
+
+type_to_field = {
+    types.AbstractType: text_field,
+    types.BLOB: text_field,
+    types.BOOLEAN: check_box,
+    types.Binary: text_field,
+    types.Boolean: text_field,
+    types.CHAR: text_field,
+    types.CLOB: text_field,
+    types.DATE: text_field,
+    types.DATETIME: text_field,
+    types.DECIMAL: text_field,
+    types.Date: text_field,
+    types.DateTime: text_field,
+    types.FLOAT: text_field,
+    types.Float: text_field,
+    types.INT: text_field,
+    types.INTEGER: text_field,
+    types.Integer: text_field,
+    types.MutableType: text_field,
+    types.NCHAR: text_field,
+    types.NULLTYPE: text_field,
+    types.NullTypeEngine: text_field,
+    types.Numeric: text_field,
+    types.PickleType: text_field,
+    types.SMALLINT: text_field,
+    types.SmallInteger: text_field,
+    types.Smallinteger: text_field,
+    types.String: text_field,
+    types.TEXT: text_area,
+    types.TIME: text_field,
+    types.TIMESTAMP: text_field,
+    types.Time: text_field,
+    types.TypeDecorator: text_field,
+    types.TypeEngine: text_field,
+    types.Unicode: text_field,
+    types.VARCHAR: text_field,
+}
+max_cols = 40
+default_rows = 5
+string_types = (types.CHAR, types.String, types.Unicode,
+                  types.VARCHAR)
+small_int_types = (types.SMALLINT, types.SmallInteger,
+                     types.Smallinteger)
+int_types = (types.INT, types.INTEGER, types.Integer)
 
 
 def make_nav_menu(context, id='nav', menu_class='menu'):
@@ -88,7 +141,8 @@ def nav_list_for_collection(member_name, parent_name=None, parent_id=None):
 
 
 def nav_list_for_member(member_name, member_id, parent_name=None,
-                        parent_id=None):
+                        parent_id=None, terse_link_text=False,
+                        join_with=None):
     if parent_name is not parent_id is not None:
         url_args = {'id': member_id, '%s_id' % parent_name: parent_id}
         show_url = '%s_%s' % (parent_name, member_name)
@@ -100,13 +154,107 @@ def nav_list_for_member(member_name, member_id, parent_name=None,
         edit_url = 'edit_%s' % member_name
         delete_url = '%s' % member_name
     m_name_title = member_name.title()
-    show_link_text = 'Show %s' % m_name_title
-    edit_link_text = 'Edit %s' % m_name_title
-    delete_link_text = 'Delete %s' % m_name_title
-    return [
-        {'a': link_to(show_link_text, url(show_url, **url_args))},
-        {'a': link_to(edit_link_text, url(edit_url, **url_args))},
+    if terse_link_text:
+        show_link_text = 'Show'
+        edit_link_text = 'Edit'
+        delete_link_text = 'Delete'
+    else:
+        show_link_text = 'Show %s' % m_name_title
+        edit_link_text = 'Edit %s' % m_name_title
+        delete_link_text = 'Delete %s' % m_name_title
+    links = [
+        {'a': link_to(show_link_text, url(show_url, **url_args),
+                      title='Show %s details' % member_name)},
+        {'a': link_to(edit_link_text, url(edit_url, **url_args),
+                      title='Edit %s details' % member_name)},
         {'a': button_to(delete_link_text, url(delete_url, **url_args),
-                        style='display: inline', method='DELETE',
-                        confirm='Are you sure?')},
+                        method='DELETE', confirm='Really delete this item?',
+                        title='Remove %s from database' % member_name)},
     ]
+    if join_with is not None:
+        links = [link['a'] for link in links]
+        links = ' &middot; '.join(links)
+    return links
+
+
+def form_field_for_col(col, member, **kw):
+    """Return a form field corresponding to the type of column ``col``.
+
+    ``member``
+        A member object of some resource
+
+    """
+    col_name = col.key
+    col_type = col.type
+    value = getattr(member, col_name, '')
+    field_maker = type_to_field.get(col_type, text_field)
+    field_args = {}
+    length = getattr(col_type, 'length', 0)
+    if (field_maker is text_field and
+        isinstance(col_type, string_types) and
+        (length > max_cols or length is None)):
+        field_maker = text_area
+    if field_maker is text_field:
+        if isinstance(col_type, string_types):
+            field_args['size'] = min(max_cols, length) or max_cols
+        elif isinstance(col_type, int_types):
+            field_args['size'] = 10
+        elif isinstance(col_type, small_int_types):
+            field_args['size'] = 5
+    elif field_maker is text_area:
+        field_args.update({'rows': default_rows, 'cols': max_cols})
+    elif field_maker is check_box:
+        if isinstance(col_type, types.BOOLEAN):
+            field_args['checked'] = value
+    # User's args override any args calculated here
+    field_args.update(kw)
+    form_field = field_maker(col_name, value, **field_args)
+    return form_field
+
+
+def nice_name_for_col(col):
+    """Return a nice, readable name for the column ``col``.
+
+    Replace "_" and "-" with " " and title-ize. Assumes column names use
+    under_score or sl-ug style. [Question: is slug-style legal for table
+    names?]
+
+    """
+    replacements = {' ': '_-'}
+    name = col.name
+    for new_val in replacements:
+        old_vals = replacements[new_val]
+        for o in old_vals:
+            name = name.replace(o, new_val)
+    name = name.title()
+    return name
+
+
+def get_prev_next_per_page(paginator):
+    """Return previous and next page numbers and number of items per page.
+
+    ``paginator``
+        Object containing pagination data, typically from a call to
+        ``webhelpers.pagination.paginate``, but could be any object with
+        ``current_page``, ``items_per_page``, and ``item_count`` attributes.
+
+    """
+    page = paginator.current_page
+    per_page = paginator.items_per_page
+    item_count = paginator.item_count
+    prev_page = page - 1
+    if prev_page < 0:
+        prev_page = 0
+    next_page = page + 1
+    last_page = item_count / per_page
+    if (item_count % per_page) == 0:
+        last_page -= 1
+    if next_page > last_page:
+        next_page = last_page
+    return prev_page, next_page, per_page
+
+
+def print_attrs(obj):
+    for a in dir(obj):
+        if not a.startswith('_'):
+            print a
