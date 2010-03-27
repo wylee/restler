85fb82fb44a7576bfd0285647c4ca58cab9fb80d
Wyatt Lee Baldwin <baldwinw@trimet.org>
2010-03-27 16:09:39 -0700

    Added test for legacy dict format of fields for simple object.

--- restler/tests/__init__.py
+++ restler/tests/__init__.py
@@ -29,6 +29,18 @@ class Fields_for_Simple_Object(unittest.TestCase):
         expected_fields = self.default_set - set([('id_str', 'id_str')])
         self.assertEqual(fields, expected_fields)
 
+    def should_work_with_legacy_dict_format(self):
+        fields = self.entity._parse_fields_for_simple_object({
+            '*': '*', 
+            'a': 'a',
+            '+b': 'B',
+            'b': 'B',
+            '-id_str': '-id_str'
+        })
+        expected_fields = self.default_set.union(set([('a', 'a'), ('b', 'B')]))
+        expected_fields -= set([('id_str', 'id_str')])
+        self.assertEqual(fields, expected_fields)
+
 
 class TestController(unittest.TestCase):
     pass
