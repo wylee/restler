03743639b1d9494ffed36ea2b9ba4283924eb017
Wyatt Lee Baldwin <baldwinw@trimet.org>
2010-03-29 13:55:13 -0700

    Updated project metadata.

--- setup.py
+++ setup.py
@@ -7,17 +7,13 @@ version = '0.5.4dev'
 setup(
     name='Restler',
     version=version,
-    description="""RESTful base controller for Pylons 0.9.7.""",
+    description="""RESTful base controller for Pylons 1.0.""",
     long_description="""\
 Restler is a controller for Pylons projects that provides a set of default
 RESTful actions that can be overridden as needed.
 
 The Restler project is hosted at Google Code. Please see
 http://code.google.com/p/restler/ for more details, documentation, etc.
-
-The latest development version can be found here:
-
-http://restler.googlecode.com/svn/trunk/#egg=Restler-dev
 
 Restler was originally extracted from the byCycle bicycle trip planner
 (http://bycycle.org).
@@ -29,11 +25,10 @@ Restler was originally extracted from the byCycle bicycle trip planner
     keywords='web pylons controller REST WSGI',
     url='http://code.google.com/p/restler/',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Pylons',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python',
